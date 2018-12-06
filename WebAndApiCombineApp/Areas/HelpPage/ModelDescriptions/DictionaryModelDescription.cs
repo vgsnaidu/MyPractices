9f827d77ecdad4ccc0d4dc1fef6418c5ec3f281d
VGS Naidu AVUGADDI <vgsnaidu.developer@outlook.com>
2018-12-07 00:12:42 +0530

    added some more content on 12/06/2018

---
+++ WebAndApiCombineApp/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAndApiCombineApp.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebAndApiCombineApp/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAndApiCombineApp.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
